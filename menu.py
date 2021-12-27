33a3f6e7dc96818ad087be678668498460eeb400
MountainDoo <64296620+MountainDoo@users.noreply.github.com>
2021-12-27 12:02:23 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,11 +1,13 @@
 __author__ = 'MountainDoo'
-__version__ = '0.1.0'
-__last_modification__ = '2021-10-15'
+__version__ = '0.1.1'
+__last_modification__ = '2021-12-27'
 import sys
-import stock_history as stk
-import stock_comparison as cmp
+import stock_history
+import stock_comparison
+import coin_history
+import coin_comparison
 
-print('Welcome to')
+print('\nWELCOME TO')
 print('      /////   //                  //      //          //')
 print('     //    /  //                  //                  //')
 print('     //      /////  ////    ////  //  //  //   ////  /////  ////')
@@ -16,34 +18,90 @@ print('          //  //   //  //  //     // //   //  //  //  //   //  //')
 print('     /    //  //   //  //  //   / //  //  //  //  //  //   //  //')
 print('      /////   //    ////    ////  //  //  //   ////   //    ////')
 
-def main_menu():
-    selection = ''
-    while selection == '':
-        print('\nMain Menu:')
+def stock_menu():
+    stock_selection = ''
+    while stock_selection == '':
+        print('\nStock Market Menu:')
         print('1) View stock history')
         print('2) Plot stock history')
         print('3) Download stock history')
         print('4) View stock comparison')
         print('5) Plot stock comparison')
         print('6) Download stock comparison')
+        print('7) Back to main menu')
         print('0) Quit')
         option = input('\nSelect an option: ')
         if option == '0':
             sys.exit()
+        elif option == '7':
+            main_menu()
         elif option == '6':
-            cmp.download_stk_cmpr()
+            stock_comparison.download_stk_cmpr()
         elif option == '5':
-            cmp.plot_stk_cmpr()
+            stock_comparison.plot_stk_cmpr()
         elif option == '4':
-            cmp.display_stk_cmpr()
+            stock_comparison.display_stk_cmpr()
         elif option == '3':
-            stk.download_stk_hist()
+            stock_history.download_stk_hist()
         elif option == '2':
-            stk.plot_stk_hist()
+            stock_history.plot_stk_hist()
         elif option == '1':
-            stk.display_stk_hist()
+            stock_history.display_stk_hist()
         else:
             print('Invalid selection.')
-    return selection
+    return stock_selection
+
+
+def coin_menu():
+    coin_selection = ''
+    while coin_selection == '':
+        print('\nCryptocurrency Menu:')
+        print('1) View coin history')
+        print('2) Plot coin history')
+        print('3) Download coin history')
+        print('4) View coin comparison')
+        print('5) Plot coin comparison')
+        print('6) Download coin comparison')
+        print('7) Back to main menu')
+        print('0) Quit')
+        option = input('\nSelect an option: ')
+        if option == '0':
+            sys.exit()
+        elif option == '7':
+            main_menu()
+        elif option == '6':
+            coin_comparison.download_coin_cmpr()
+        elif option == '5':
+            coin_comparison.plot_coin_cmpr()
+        elif option == '4':
+            coin_comparison.display_coin_cmpr()
+        elif option == '3':
+            coin_history.download_coin_hist()
+        elif option == '2':
+            coin_history.plot_coin_hist()
+        elif option == '1':
+            coin_history.display_coin_hist()
+        else:
+            print('Invalid selection.')
+    return coin_selection
+
+def main_menu():
+    menu_selection = ''
+    while menu_selection == '':
+        print('\nMain Menu:')
+        print('1) Stock Market Data')
+        print('2) Cryptocurrency Data')
+        print('0) Quit')
+        option = input('\nSelect an option: ')
+        if option == '0':
+            sys.exit()
+        elif option == '2':
+            coin_menu()
+        elif option == '1':
+            stock_menu()
+        else:
+            print('Invalid selection.')
+    return menu_selection
+
 
 main_menu()
